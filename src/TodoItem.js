8bb16278299dc566d07cfebe6b54a7effb8d58ea
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-07-08 11:33:43 -0400

    Mark a Todo as completed and Deleted

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import './TodoItem.css';
 function TodoItem(props){
-const onComplete = () => {
-    alert(`Task ${props.text} is done`);
-    props.completed=true;
-}
-const onDelete = () => {
-    alert(`Erasing task ${props.text}`);
-    // props.completed=true;
-}
 
 return (
     <li className="TodoItem">
         <span 
         className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-        onClick={onComplete}
+        onClick={props.onComplete}
         
         >√</span>
         <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
         <span  
         className="Icon Icon-delete"
-        onClick={onDelete}
+        onClick={props.onDelete}
         
         >X</span>
     </li>
