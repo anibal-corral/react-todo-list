02b3f02d04e974ac83c101f36f4c620314b94bc0
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-07-08 10:36:52 -0400

    TodoSearch element has state property

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import './TodoSearch.css'
 function TodoSearch(){
+  //SearchValue is empty.
+  const [searchValue, setSearchValue] = React.useState('');
+
   const onSearchValueChange = (event) => {
-    console.log(event.target.value);
+    //When user type something, this value is set to searchValue property.
+    setSearchValue(event.target.value);
   };
 
     return(
+      <React.Fragment>
       <input  
       className="TodoSearch" 
       placeholder="Onion"
+      //set the input value with searchValue value.
+      value={searchValue}
       onChange={onSearchValueChange}
       />
+      <p>{searchValue}</p>
+      </React.Fragment>
     )
 }
+
 
 export {TodoSearch}
